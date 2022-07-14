3afafeb2eff8ba4d6612ab80680886bd8951824b
yuthegreat1 <68827740+yuthegreat1@users.noreply.github.com>
2022-07-13 22:29:00 -0500

    finished the pypoll and removed unecessary files

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,8 +1,29 @@
 import pandas as pd
 file = 'Resources/election_data.csv'
 df = pd.read_csv(file)
+#finding the file 
 total_votes = (len(df))
+#finding total votes
 candidate_names = df["Candidate"].unique()
-candidate_grouped = df.groupby(["Candidate"]
+#series of names of candidataes
+candidate_grouped = df.groupby(["Candidate"])
 candidate_total = candidate_grouped.count()
+#group by candidate and total the votes
 total = candidate_total.loc[["Charles Casper Stockham","Diana DeGette","Raymon Anthony Doane"],"Ballot ID"]
+#this seems useless but my code errors when i don't inclue it
+sum = total.sum()
+percentage = (total*100/sum).map("{:,.2f}%".format)
+#formatting percentage
+ordered_totals = candidate_total.sort_values("Ballot ID",ascending=False)
+#sort to find the winner
+winner = str(ordered_totals.iloc[0])
+#print results
+print("Election Results")
+print("-------------------------")
+print("Total Votes: " + str(total_votes))
+print("-------------------------")
+print(percentage)
+print("-------------------------")
+print("Winner: "+ winner)
+print("-------------------------")
+
