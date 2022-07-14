4bf2008957aed2d109ce682d34eb56e9795ba068
yuthegreat1 <68827740+yuthegreat1@users.noreply.github.com>
2022-07-13 23:58:44 -0500

    added comments to pybank

--- PyBank/main.py
+++ PyBank/main.py
@@ -13,27 +13,38 @@ with open(file) as csvfile:
     i = 0
     total = 0
     store = []
+    #intialize the variables
     for row in csvreader:
         total += int(row[1])
         i += 1
         store.append([row[0], int(row[1])])
+        #make the values into integers and create a list to store them
 
 previous = store[0][1]
+#create a value that is considered the lowest
 increase = [0, store[0][1]]
 decrease = [0, store[0][1]]
+#initilize vairbles
 change = []
 for i in range(len(store)):
     change.append(store[i][1] - previous)
+    #find the change in the profit
     if store[i][1] > increase[1]:
         increase[0] = store[i][0]
         increase[1] = store[i][1]
     if store[i][1] < decrease[1]:
         decrease[0] = store[i][0]
         decrease[1] = store[i][1]
+    #if they are greater or lower replace the
     previous = store[i][1]
 avg_change = np.mean(change[1:])
-
-
+#print analysis
+print("Financial Analysis\n")
+print("----------------------------\n")
+print("Total Months:" + str(len(store))+"\n")
+print("Total: $"+str(total)+"\n")
+print("Greatest Increase in Profits:"+str(increase)+"\n")
+print("Greatest Decrease in Profits:"+str(decrease)+"\n")
 with open('analysis.txt', 'w') as f:
     f.write("Financial Analysis\n")
     f.write("----------------------------\n")
